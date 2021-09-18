88595d56b7dd99a3a2e009891e67933cf0e5353f
Angela <angmayorga@gmail.com>
2021-09-18 14:40:10 -0500

    Se agrega atributo de empleado

--- Dominio/Entidades/Empleado.cs
+++ Dominio/Entidades/Empleado.cs
@@ -1,7 +1,9 @@
 namespace Dominio.Entidades
 {
-    public class Empleado
+    public class Empleado : Persona
     {
+        public Empleado(string id, string nombre, string primerApellido, string segundoApellido, DateTime fechaNacimiento, string documento) : base(id, nombre, primerApellido, segundoApellido, fechaNacimiento, documento){   
+        }
         public int SueldoBruto { get; set; }
     }
 }
