02aa19c56679b7cace33cd9190bbaacf51806626
cofla1347 <haroldeduardoramirez@gmail.com>
2021-09-18 21:22:07 -0500

    Se implementa clase Empresa con sus atributos

--- Dominio/Entidades/Empresa.cs
+++ Dominio/Entidades/Empresa.cs
@@ -2,6 +2,10 @@ namespace Dominio.Entidades
 {
     public class Empresa
     {
-        
+        public int id { get; set; }
+        public string RazonSocial { get; set; }
+        public string Nit { get; set; }
+        public string Direccion { get; set; }       
+
     }
 }
