335341908f401629346b2a49beac54359ddfe96e
cofla1347 <haroldeduardoramirez@gmail.com>
2021-09-25 23:24:21 -0500

    Implementación CRUD para la enitdad Empresa

---
+++ Persistencia/AppRepositorios/IRepositorioEmpresa.cs
@@ -0,0 +1,15 @@
+using Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace Persistencia.AppRepositorios
+{
+    public interface IRepositorioEmpresa
+    {
+        Empresa AgregarEmpresa(Empresa empresa);
+        Empresa ActualizarEmpresa(Empresa empresa);
+        void EliminarEmpresa(int idEmpresa);
+        Empresa ObtenerEmpresa(int idEmpresa);
+        IEnumerable<Empresa> ObtenerEmpresas();
+
+    }
+}
