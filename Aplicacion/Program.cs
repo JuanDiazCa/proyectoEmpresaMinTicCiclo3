61118585a415c2785aca5524936e71c2564629a5
JuanDiazCa <judiazca2@poligran.edu.co>
2021-09-26 20:30:54 -0500

    Prueba crud Empresa

--- Aplicacion/Program.cs
+++ Aplicacion/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using Dominio;
+using Persistencia;
 
 namespace Aplicacion
 {
     class Program
     {
+        private static IRepositorioEmpresa _repoEmpresa =  new RepositorioEmpresa(new Persistencia.AppRepositorios.AppContext());
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Team D Desarroladores!");
+            Console.WriteLine("PRUEBAS DE CRUD");
+            pruebaCrud();
+        }
+        
+        private static pruebaCrud()
+        {
+            var empresa = new Empresa{
+                RazonSocial = "Empresa XYZ",
+                Nit = "123456-89",
+                Direccion = "CL 123 85 96"
+            };
+            Console.WriteLine("Agregar empresa");
+            _repoEmpresa.AgregarEmpresa(empresa);
         }
     }
 }
