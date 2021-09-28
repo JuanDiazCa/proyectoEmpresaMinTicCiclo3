17b1b0c9d7a3bbef2d9d2baf2b73dfa6a801304b
JuanDiazCa <judiazca2@poligran.edu.co>
2021-09-27 23:09:44 -0500

    Creacion de capa razor para el taller (consultas)

---
+++ FrontEndTaller/Pages/Privacy.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dominio.Entidades;
+using Persistencia.AppRepositorios;
+
+namespace MyApp.Namespace
+{
+    public class ListModel : PageModel
+    {
+        private static IRepositorioCliente _repoCliente;
+        //private static IRepositorioCliente _repoCliente =  new RepositorioCliente(new Persistencia.AppRepositorios.AppContext());
+        //private IEnumerable<Cliente> Clientes {get; set;}
+        private string[] nombres = {"cliente 1", "cliente 2", "cliente 3"};
+        public List<String> Clientes {get; set;}
+
+        public ListModel()
+        {
+            //this._repoCliente = _repoCliente;
+            
+        }
+
+        public void OnGet()
+        {
+            //Clientes = _repoCliente.ObtenerTodosLosClientes();
+            Clientes = new List<string>  {"prueba","prueba2"};
+        }
+    }
+}
